ac3a24794d3a3088f9f19a168ddeb20fee4b7a94
David Charbonnier <d.charbonnier@oxys.net>
2012-03-06 17:47:14 +0100

    lazy renderer settings (wip)

--- docviewer/static/js/lazy-docviewer.js
+++ docviewer/static/js/lazy-docviewer.js
@@ -1,9 +1,7 @@
 $(window).load(function() {
 			var docviewer_index = 0;
 			var targeting = {};
-      console.log('rrrr');
 			$('.docviewer').each(function() {
-        console.log('rr');
 						if (typeof($(this).attr('id')) === 'undefined') {
 							$(this).attr('id', 'docviewer-' + docviewer_index);
 							var id = $(this).attr('id');
@@ -13,6 +11,10 @@ $(window).load(function() {
 						}
             docviewer.load('/documents/doc-'+$(this).attr('data-document')+'.json', {
 									container : '#'+id,
+                  width: '100%',
+                  height: 700,
+                  text: true,
+                  search: true,
 									sidebar : false
 								});
 					});
