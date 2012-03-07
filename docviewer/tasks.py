2c23aa3f9d2df3486acf748625e59cef9d341394
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-03-07 09:44:30 +0100

    sucks with command

--- docviewer/tasks.py
+++ docviewer/tasks.py
@@ -1,6 +1,29 @@
 from celery.task import task
 from django.core import management
+from docviewer.models import Document
+from docviewer.helpers import docsplit
 
 @task()
 def generate_document(doc_id, filepath, task_id=None):
-    management.call_command('generate_document', doc_id, file=filepath, task=task_id, verbosity=0, interactive=False)
+    
+    document = Document.objects.get(pk=doc_id)
+        
+    if document.task_id != task_id:
+        
+        raise Exception("Celery task ID doesn't match")
+    try:
+        docsplit(filepath)
+    
+        document.generate()
+        document.status = document.STATUS.ready
+        document.task_id = None
+        document.save()
+    except Exception, e:
+        
+        try:
+            document.task_error = str(e)
+            document.save()
+        except:
+            pass
+        
+        raise
