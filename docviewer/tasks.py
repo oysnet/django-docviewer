4fceadfe9e7af11a6f2748d4945a4cd06c4c7916
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-03-08 09:53:38 +0100

    retry tasks

--- docviewer/tasks.py
+++ docviewer/tasks.py
@@ -1,7 +1,11 @@
 from celery.task import task
+from django.core.exceptions import ObjectDoesNotExist
 
 
-@task()
+@task(default_retry_delay=10, max_retries=5)
 def task_generate_document(doc_id, task_id=None):
-    from docviewer.helpers import generate_document    
-    generate_document(doc_id, task_id)
+    from docviewer.helpers import generate_document  
+    try:  
+        generate_document(doc_id, task_id)
+    except ObjectDoesNotExist, e:
+        task_generate_document.retry(exc=e)
