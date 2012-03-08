4872be99eb1becdb3c72af17d3a8a61aefc21c5a
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-03-08 08:19:26 +0100

    remplace fulltext by text

--- docviewer/search_indexes.py
+++ docviewer/search_indexes.py
@@ -6,7 +6,7 @@ from docviewer.models import Page
 
 class PageIndex(RealTimeSearchIndex):
     
-    fulltext = CharField(document=True)
+    text = CharField(document=True)
     document_id =  IntegerField(model_attr='document__id')
     page = IntegerField(model_attr="page")
     
