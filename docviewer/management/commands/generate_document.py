a8cd022b5396135dce0e393a53a858dde0bac7d8
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-03-09 08:53:35 +0100

    manually generate document

--- docviewer/management/commands/generate_document.py
+++ docviewer/management/commands/generate_document.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from docviewer.models import Document
 
 from optparse import make_option
-from docviewer.helpers import docsplit
+from docviewer.helpers import docsplit, generate_document
 
 
 class Command(BaseCommand):
@@ -13,31 +13,9 @@ class Command(BaseCommand):
             dest='task_id',
             default=None,
             help='Celery task ID'),
-        make_option('--file',
-            action='store',
-            dest='filepath',
-            default=None,
-            help='full file path'),)
+       )
     
     def handle(self, *args, **options):
         
-        document = Document.objects.get(pk=args[0])
-        
-        if options.get('task_id', None) is not None and document.task_id != options.get('task_id'):
-            raise Exception("Celery task ID doesn't match")
-        try:
-            docsplit(options.get('filepath'))
-        
-            document.generate()
-            document.status = document.STATUS.ready
-            document.task_id = None
-            document.save()
-        except Exception, e:
-            
-            try:
-                document.task_error = str(e)
-                document.save()
-            except:
-                pass
-            
-            raise
+        for id in args:
+            generate_document(id)
