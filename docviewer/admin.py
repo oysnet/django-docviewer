ee4746601707df77bd2142348386ba855dc38d97
Jean-Hugues Pinson <jh.pinson@oxys.net>
2012-05-24 12:16:14 +0200

    admin: ollow selection from ckeditor

--- docviewer/admin.py
+++ docviewer/admin.py
@@ -2,8 +2,26 @@ from django.contrib import admin
 from docviewer.models import Document
 from docviewer.forms import DocumentForm
 from docviewer.tasks import task_generate_document
+from django.contrib.admin.views.main import ChangeList
 
 
+class DocViewerChangeList(ChangeList):
+    
+    def __init__(self, request, *args, **kwargs):
+        params = dict(request.GET.items())
+        
+        try:
+            del params['CKEditorFuncNum']
+        except:
+            pass
+        
+        
+        request.GET = params
+        super(DocViewerChangeList, self).__init__(request, *args, **kwargs)
+        
+   
+    
+    
 
 class DocumentAdmin(admin.ModelAdmin):
     
@@ -14,6 +32,9 @@ class DocumentAdmin(admin.ModelAdmin):
         (None, {'fields': ('title', 'description', 'file', 'source_url', 'related_url', 'contributor', 'contributor_organization', 'download')}),
         ('Meta', {'fields': ('status', 'filename', 'page_count',  'task_id', 'task_error', 'task_start')}),
     )
+    
+    def get_changelist(self, request, **kwargs):
+        return DocViewerChangeList
     
     def save_model(self, request, obj, form, change):
         obj.save()
