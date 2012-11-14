de41c886b541dbb120a6685b0c94e00bf975fad7
Roberto <roberto.ur@gmail.com>
2012-11-14 14:59:30 -0500

    Default configuration for ElasticSearch
    The configuration of haystack is ready for elasticsearch

--- demo/demoproject/settings.py
+++ demo/demoproject/settings.py
@@ -184,23 +184,23 @@ LOGGING = {
 }
 
 #Haystack Configuration for Whoosh
-import os
-HAYSTACK_CONNECTIONS = {
-	'default': {
-	    'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-	    'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
-	},
-}
-
-#Haystack Configuration for ElasticSearch
 #import os
 #HAYSTACK_CONNECTIONS = {
-#    'default': {
-#        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-#        'URL': 'http://127.0.0.1:9200/',
-#        'INDEX_NAME': 'haystack',
-#    },
+#	'default': {
+#	    'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+#	    'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+#	},
 #}
+
+#Haystack Configuration for ElasticSearch
+import os
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
+    },
+}
 
 # Celery configuration
 BROKER_URL='amqp://guest:guest@localhost:5672//'
