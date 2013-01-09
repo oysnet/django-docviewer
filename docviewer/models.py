660dedaf0b5a4183f146c7d7db7066acad32b8d5
Roberto Ulloa <roberto@laboratorio-desktop.(none)>
2013-01-09 18:13:28 -0500

    removing reindexing when the document is modified

--- docviewer/models.py
+++ docviewer/models.py
@@ -87,9 +87,6 @@ class Document(TimeStampedModel, StatusModel):
         super(Document, self).save(*args, **kwargs)
         if create:
             os.makedirs(self.get_root_path())
-        else:
-            for pag in self.pages_set.all():
-              pag.save()
 
 
     def delete(self):
