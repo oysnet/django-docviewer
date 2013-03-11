c767529d48960b5377c7e7a055189c15c054e7c8
Roberto <roberto.ur@gmail.com>
2013-03-11 15:28:35 -0300

    Old versions of celery and haystack
    For some reason the signals are not working

--- setup.py
+++ setup.py
@@ -21,11 +21,11 @@ setup(
                       'django-pipeline',
                       #'django-celery',
                       'django-celery',
-                      'celery-haystack'],
-                      #'celery-haystack==2.0.0-stefanw'],
+                      #'celery-haystack'],
+                      'celery-haystack==0.6.2-old'],
     dependency_links = [
-        'http://github.com/toastdriven/django-haystack/tarball/6065c8525bea4b9a116c7fa279b9e3e26ac7362f#egg=django-haystack-2.0.0-beta'
-        #'http://github.com/stefanw/celery-haystack/tarball/signal-processor#egg=celery-haystack-2.0.0-stefanw'
+        'http://github.com/toastdriven/django-haystack/tarball/6065c8525bea4b9a116c7fa279b9e3e26ac7362f#egg=django-haystack-2.0.0-beta',
+        'http://github.com/jezdez/celery-haystack/tarball/795d37450815c680e4f936f2ba2ff77d882dd3a0#egg=celery-haystack-0.6.2-old'
     ],
     classifiers=[
         'Environment :: Web Environment',
