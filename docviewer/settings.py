df0dff89ef1886e91d7f75c26db2b294ade7a924
Jean-Hugues Pinson <jh.pinson@oxys.net>
2013-09-03 16:59:11 +0200

    introduce DOCVIEWER_HAYSTACK_CONNECTION to define which haystack connection to use

--- docviewer/settings.py
+++ docviewer/settings.py
@@ -4,3 +4,4 @@ from django.conf import settings
 DOCUMENT_ROOT = getattr(settings, "DOCVIEWER_DOCUMENT_ROOT", "/docs/")
 DOCUMENT_URL = getattr(settings, "DOCVIEWER_DOCUMENT_URL", "/docs/")
 IMAGE_FORMAT = getattr(settings, "DOCVIEWER_IMAGE_FORMAT", "png")
+HAYSTACK_CONNECTION = getattr(settings, "DOCVIEWER_HAYSTACK_CONNECTION", "default")
